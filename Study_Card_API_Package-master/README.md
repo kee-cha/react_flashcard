9377dd71e96a77a92af876f20dd48c2824f4d95a
kee cha <kee_cha18@yahoo.com>
2020-02-22 18:05:03 -0600

    init

---
+++ Study_Card_API_Package-master/README.md
@@ -0,0 +1,2 @@
+# Study_Card_API_Package
+Seeded Study Card Data API for React Project.
---
+++ src/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React, { Componet } from 'react';
+import './Card.css';
+
+const Card = (props) => (
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className=""           
+            </div>
+            <div className="back">
+            </div>
+        </div>
+    </div>
+)
+
+
